8bcfe9353f714f8e0533872910731370727ac29e
Lightnet <darkneter@gmail.com>
2023-03-14 15:48:01 -0700

    rework the layout.
    Added fps config, window mode, rework the layouts and others.

--- scripts/Settings.gd
+++ scripts/Settings.gd
@@ -4,22 +4,68 @@ extends MarginContainer
 
 @onready var UISettings = $"."
 
+@onready var NCurrentFPS = $VBoxContainer/Panel/TabContainer/Graphic/GridContainer/LCurrent_FPS
+@onready var NVSync = $VBoxContainer/Panel/TabContainer/Graphic/GridContainer/CB_Vsync
+@onready var NWindowMode = $VBoxContainer/Panel/TabContainer/Graphic/GridContainer/OB_WindowModes
+
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
+func _ready():
+	print("DisplayServer vsync_mode: ", DisplayServer.window_get_vsync_mode())
+	if DisplayServer.window_get_vsync_mode() == 1:
+		NVSync.button_pressed = true
+	else:
+		NVSync.button_pressed = false
+	pass 
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
+func _process(_delta):
+	if NCurrentFPS:
+		NCurrentFPS.text = str(Engine.get_frames_per_second())
+		pass
+	pass
 # get_tree().root.content_scale_mode
-
-
-#Engine.max_fps
 
 func _on_btn_back_pressed():
 	UISettings.hide()
-	pass # Replace with function body.
-
+	pass
 
 func _on_btn_screen_size_pressed():
 	print(DisplayServer.screen_get_size())
+	pass
+
+func _on_spin_b_fps_value_changed(value):
+	var fps = int(value)
+	if fps:
+		Engine.max_fps = fps
+	pass
+
+func _on_cb_vsync_toggled(button_pressed):
+	#print("button_pressed: ", button_pressed)
+	#var displayid = DisplayServer.window_get_current_screen()
+	if button_pressed:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	pass 
+
+func _on_mb_window_mode_pressed():
+	
+	pass # Replace with function body.
+
+
+func _on_ob_window_modes_item_selected(index):
+	print("index: ",index)
+	#var displayid = DisplayServer.window_get_current_screen()# display monitor
+	#print("displayid: ", displayid)
+	var primaryid = DisplayServer.get_primary_screen()
+	print("primaryid: ", primaryid)
+	var windowmode = DisplayServer.window_get_mode(primaryid)
+	print("windowmode: ", windowmode)
+	if index == 0:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED, primaryid)
+	if index == 1:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED , primaryid)
+	if index == 2:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN , primaryid)
+	
 	pass # Replace with function body.
